7937ac8fc5fd69f1af68001e2d2cec5fa8e3be23
RowanWang <RowanWang@foxmail.com>
2022-07-20 15:48:06 +0800

    第一次提交2022/07/20/15.47

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: '中银金科发票报销',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
